d469c0c081930d588b49d9d62fcfe787da34a0bb
unknown <omerfarukkirikkaya@gmail.com>
2019-08-26 17:57:18 +0300

    Menu-item componenti için router yapısı kuruldu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.componet";
 
+const HatsPage = () => {
+  return <div>Hats</div>;
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
